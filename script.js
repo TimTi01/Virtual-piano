c605a569d9e174293fd49f2d43da1444f542bbea
Artem <TimF1234@yandex.ru>
2021-05-02 23:08:50 +0300

    rework entire design

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 const whitePianoKeys = findAllWhitePianoKeys();
 bindClickListenerWhiteKeys(whitePianoKeys);
-
 const blackPianoKeys = findAllBlackPianoKeys();
 bindClickListenerBlackKeys(blackPianoKeys);
 
+// slider
+let position = 0;
+let wrapSlider = document.body.querySelector('.container-picture');
+let wrapSliderWidth = wrapSlider.offsetWidth; //290px
+let slider = document.body.querySelector('.slider-track');
+let sliderWhidth = slider.offsetWidth;
+
+let picturesWhidth = wrapSliderWidth;
+
+// slider-bttns-start
+const leftSliderKey = findLeftSliderKeys();
+bindClickListenerSlidBttnLeft(leftSliderKey);
+const rightSliderKey = findRightSliderKey();
+bindClickListenerSlidBttnRight(rightSliderKey);
+// slider-bttns-end
 
 
 function findAllWhitePianoKeys() {
@@ -33,5 +47,39 @@ function bindClickListenerBlackKeys(pianoKeys) {
             let audio = document.body.querySelector(`[data-key-num='${dataNum}']`);
             audio.play();
         });
+    });
+}
+
+
+
+// slider-functions-start
+
+function findLeftSliderKeys() {
+    let leftButton = document.body.querySelector('.slider_button-left');
+    return leftButton;
+}
+
+function bindClickListenerSlidBttnLeft(bttn) {
+    bttn.addEventListener('click', (bttn) => {
+        position -= picturesWhidth;
+
+        changPosition = slider.style.transform = `translateX(${-picturesWhidth}px)`;
+        console.log(position);
+    });
+}
+
+
+
+function findRightSliderKey() {
+    let rightButton = document.body.querySelector('.slider_button-right');
+    return rightButton;
+}
+
+function bindClickListenerSlidBttnRight(bttn) {
+    bttn.addEventListener('click', (bttn) => {
+        position += picturesWhidth;
+
+        changPosition = slider.style.transform = `translateX(${picturesWhidth}px)`;
+        console.log(position);
     });
 }
